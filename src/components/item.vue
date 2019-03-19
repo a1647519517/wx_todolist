4103695728675e25a2ec6df0343486611dc9cdbe
a1647519517 <wangbin15955398935@gmail.com>
2019-03-19 09:52:20 +0800

    no message

---
+++ src/components/item.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="item">
+    <div class="content">
+      <div class="time-area">
+        <div class="icon"></div>
+        <div class="time">
+          <span>{{time}}</span>
+        </div>
+      </div>
+      <div class="note-area">
+        <div class="note">
+          <span>{{note}}</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'list_item',
+  props: ['time', 'note'],
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .item {
+    height: 150rpx;
+    width: 100%;
+    margin-top: 5rpx;
+    background-color: grey;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  .content {
+    height: 100%;
+    width: 80%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+</style>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="container">
+    <div class="head">
+      <div class="lhead">
+        <div class="month" style="text-align:right;font-size:40px;">
+          <span>{{day}}</span>
+        </div>
+      </div>
+      <div class="rhead">
+        <div class="month-en" style="text-align:left;font-size:20px;">
+          <span>{{data}}</span>
+          <br>
+          <span>{{today}}</span>
+        </div>
+      </div>
+    </div>
+    <div class="body">
+      <div class="data-scroll">
+      </div>
+      <item></item>
+    </div>
+  </div>
+</template>
+
+<script>
+import item from '@/components/item.vue'
+
+export default {
+  data () {
+    return {
+      day: 19,
+      data: '2019年3月',
+      today: 'Wednesday'
+    }
+  },
+
+  components: {
+    item
+  },
+
+  methods: {
+  },
+
+  created () {
+    // let app = getApp()
+  }
+}
+</script>
+
+<style scoped>
+  .container {
+    padding: 0rpx;
+    margin: 0rpx;
+    position:absolute;
+    top: 0rpx;
+    left: 0rpx;
+    width:100%;
+    height:100%;
+    border:hidden;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+  }
+
+  .head {
+    height: 25%;
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+  }
+
+  .lhead{
+    height: 100%;
+    width: 30%;
+    background-color: #FFEC8B;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-content: center;
+  }
+
+  .rhead {
+    height: 100%;
+    width: 70%;
+    background-color: #FFEC8B ;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-content: center;
+  }
+
+  .month, .month-en {
+    width: 90%;
+    height: 40px;
+    color: white;
+  }
+
+  .body {
+    height: 75%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-content: center;
+  }
+
+  .data-scroll {
+    height: 10%;
+    width: 100%;
+    background-color: dodgerblue;
+  }
+</style>
